3cf7e8603cda36012672360529bf4d799f5f641e
unknown <a16oren@gmail.com>
2020-09-19 19:51:53 +0300

    5000 and few commetes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer({dest: __dirname + '/uploads'});
+// var bodyParser = require('body-parser');
+// var multer = require('multer');
+// var upload = multer({dest: __dirname + '/uploads'});
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 
 
 app.use('/public', express.static(__dirname + '/public'));
