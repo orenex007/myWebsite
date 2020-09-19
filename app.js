5a410badf6b924e31dd81dd44260767bdffc1d63
unknown <a16oren@gmail.com>
2020-09-19 19:28:33 +0300

    add all them to package.json

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({dest: __dirname + '/uploads'});
+var app = express();
+var port = 3000;
+
+
+app.use('/public', express.static(__dirname + '/public'));
+// app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/dinofit.html')
+    //  res.send("hello!");
+})
+
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+    // res.send("world!");
+})
+
+// app.get("/index", function(req, res){
+//   res.writeHead(200, {'Content-Type': 'image/png'});
+//   res.end("fit.jpg");
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
