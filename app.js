7fcfcc923e143d177d2aefe52fe37720881b6ec2
unknown <a16oren@gmail.com>
2020-09-19 19:56:59 +0300

    FUCKING 3000!!!!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 // var multer = require('multer');
 // var upload = multer({dest: __dirname + '/uploads'});
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 
 
 app.use('/public', express.static(__dirname + '/public'));
