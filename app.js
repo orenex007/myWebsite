3e104069ca2ff04f9bfc7e8fb10399233a53f6a3
unknown <a16oren@gmail.com>
2020-11-17 11:31:23 +0200

    my first commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require ('path');
 // var bodyParser = require('body-parser');
 // var multer = require('multer');
 // var upload = multer({dest: __dirname + '/uploads'});
@@ -6,25 +7,19 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 // app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/dinofit.html')
-    //  res.send("hello!");
-})
-
-
-app.get('/index', (req, res) => {
+// app.get("/", function (req, res){
+//   res.send("hello!");
+// })
+app.get('/', function(req, res){
     res.sendFile(__dirname + "/views/index.html");
     // res.send("world!");
 })
 
-// app.get("/index", function(req, res){
-//   res.writeHead(200, {'Content-Type': 'image/png'});
-//   res.end("fit.jpg");
-// })
-
-app.listen(PORT, () => {
+app.listen(PORT, function(){
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
+
+// “app.use("/images", express.static(path.join(__dirname, "images")));” 
---
+++ public/javascript/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$(".navbar").addClass("sticky");
+		}
+		else{
+			$(".navbar").removeClass("sticky");
+		}
+		if(this.scrollY > 500){
+			$(".scroll-up-btn").addClass("show");
+		}
+		else{
+			$(".scroll-up-btn").removeClass("show")
+		}
+	});
+
+	//slide-up script
+	$(".scroll-up-btn").click(function(){
+		$("html").animate({scrollTop: 0});
+	});
+	// toogle menu/navbar script 
+	$(".menu-btn").click(function(){
+		$(".navbar .menu").toggleClass("active");
+		$(".menu-btn i").toggleClass("active");
+	});
+	//typing animation script(from website of cdnjs)
+	var typed = new Typed(".typing-1", {
+		strings: ["Websiter", "Developer", "Front-end", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});	
+	var typed = new Typed(".typing-2", {
+		strings: ["Websiter", "Developer", "Front-end", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+});
+
+
+/*to APP!!! app.use(express.static('/public'));*** https://stackoverflow.com/questions/49635253/image-not-found-in-webpage-served-with-node-js-and-express   https://www.youtube.com/watch?v=BNfmCdec1es*/
